636becd84b82881aabb252c21e65f3f9497a7246
elifozcakmak <70036208+elifozcakmak@users.noreply.github.com>
2021-02-21 23:39:03 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,20 @@
-#Explain your work
+students=[]
+grades=[]
+info=dict()
+sortedinfo=dict()
 
-#Question 1
-for x in range(a):
-	print(a)
+for i in range(5):
+  students.append(input("Name-Surname: "))
+  tmp=[]
+  tmp.append(int(input("Midterm: ")))
+  tmp.append(int(input("Final: ")))
+  tmp.append(int(input("Homework: ")))
+  average=(tmp[0]+tmp[1]+tmp[2])/3
+  tmp.append(average)
+  grades.append(tmp)
+  info.update({students[i]:grades[i]})
+  #print(info)
+
+sortedinfo=dict(sorted(info.items(), key=lambda item: item[1][3], reverse=True))
+student_highestgrade=list(sortedinfo.keys())[0]
+print("Congratulations " + student_highestgrade +", you got the highest score!")
