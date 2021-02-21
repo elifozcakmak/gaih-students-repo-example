60bdd5348ca13aa4fa5576695897d725e7f292d4
elifozcakmak <70036208+elifozcakmak@users.noreply.github.com>
2021-02-21 23:37:41 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,19 @@
-#Explain your work
+import random
+primes=[]
 
-#Question 1
-for x in range(a):
-	print(a)
+for i in range(1,100):
+  if i > 1:
+    for j in range(2,i):
+      if (i % j) == 0:
+        break
+    else:
+        primes.append(i)
+
+row=range(3)
+matrix=[]
+for i in range(3):
+  row=[]
+  for j in range(3):
+    row.append(random.choice(primes))
+  matrix.append(row)
+  print(matrix[i])
