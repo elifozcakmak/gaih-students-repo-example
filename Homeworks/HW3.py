c3468425ea9ba9897a73400832eb6c00a3c6f42d
elifozcakmak <70036208+elifozcakmak@users.noreply.github.com>
2021-02-21 23:39:52 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,25 @@
-#Explain your work
+def prime_first(val):
+  primes=[]
+  for i in range(val,val+1):
+    if i > 1:
+      for j in range(2,i):
+        if (i % j) == 0:
+          break
+      else:
+        print(i)
 
-#Question 1
-for x in range(a):
-	print(a)
+def prime_second(val):
+  primes=[]
+  for i in range(val,val+1):
+    if i > 1:
+      for j in range(2,i):
+        if (i % j) == 0:
+          break
+      else:
+        print(i)
+
+for x in range(1000):
+    if x<500:
+      prime_first(x)
+    else :
+      prime_second(x)
